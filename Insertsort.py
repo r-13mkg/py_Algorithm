14606bc3b12add24b18e49e644230a383adf4222
r-13mkg <yur.mpc@live.jp>
2018-09-18 22:34:15 +0900

    InsertSort

--- Insertsort.py
+++ Insertsort.py
@@ -1,6 +1,21 @@
-def InsertSort():
-    print("Insert Sort:")
 
-InsertSort()
 
-InsertSort()
+
+def InsertSort(SortArray):
+    for i in range(1,len(SortArray)):
+        Insert(SortArray, i)
+
+def Insert(SortArray, i):
+    temp = SortArray[i]
+    for LowColumn in range(i - 1, -1, -1):
+        if temp < SortArray[LowColumn]:
+            SortArray[LowColumn + 1] = SortArray[LowColumn]
+        else:
+            SortArray[LowColumn + 1] = temp
+            break
+    else:
+        SortArray[0] = temp
+            
+A = [6,3,8,5,7,1,9]
+InsertSort(A)
+print(A)
